9a5498f056a644368a5ff83d214569df669c8620
Dynosaur <heracassia@gmail.com>
2020-04-15 02:05:32 -0500

    Created transformation scripts for camera and player.

---
+++ Assets/Scripts/CameraTransformer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CameraTransformer : MonoBehaviour {
+
+    private Vector3 cameraOffset;
+    private GameObject player;
+
+    private int maxCameraDepression = -45;
+    private int maxCameraElevation = 45;
+
+    public void Start() {
+        cameraOffset = new Vector3(0, 3, 0);
+        player = GameObject.Find("Player");
+    }
+
+    public void Update() {
+        transform.position = player.transform.position + cameraOffset;
+        Debug.Log(transform.rotation.z);
+        RotateVertical(Input.GetAxis("Mouse Y"));
+        RotateHorizontal(Input.GetAxis("Mouse X"));
+    }
+
+    private void RotateVertical(float amount) {
+        if (amount > 0 && -transform.rotation.x * 180 >= maxCameraElevation) {
+            return;
+        }
+        if (amount < 0 && -transform.rotation.x * 180 <= maxCameraDepression) {
+            return;
+        }
+        Quaternion q = transform.rotation;
+        q.eulerAngles = new Vector3(q.eulerAngles.x - amount, q.eulerAngles.y, 0);
+        transform.rotation = q;
+    }
+
+    private void RotateHorizontal(float amount) {
+        Quaternion q = transform.rotation;
+        q.eulerAngles = new Vector3(q.eulerAngles.x, q.eulerAngles.y + amount, 0);
+        transform.rotation = q;
+    }
+
+}
---
+++ Assets/Scripts/PlayerTransformer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerTransformer : MonoBehaviour {
+
+    private float playerSpeed = 0.01F;
+
+    public void Update() {
+        Forward(Input.GetAxis("Vertical"));
+        Sideways(Input.GetAxis("Horizontal"));
+        RotateHorizontal(Input.GetAxis("Mouse X"));
+    }
+
+    private void Forward(float amount) {
+        transform.Translate(Vector3.forward * amount * playerSpeed);
+    }
+
+    private void Sideways(float amount) {
+        transform.Translate(Vector3.right * amount * playerSpeed);
+    }
+
+    private void RotateHorizontal(float amount) {
+        transform.Rotate(Vector3.up * amount);
+    }
+}
